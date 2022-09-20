42a04768cf8253c5e88537f79478346ea610eb89
Coder Udu <uduvinci@gmail.com>
2022-09-20 02:37:38 +0100

    FormHandler

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import ( "fmt"
+"net/http"
+"log" )
+
+func  formHandler(w http.ResponseWriter, r *http.Request){
+if err:=r.ParseForm() ; err !=nil{
+	fmt.Fprintf(w, "ParseForm() err:%v", err)
+	return
+}
+fmt.Fprintf(w,"post request successful")
+name:=r.FormValue("name")
+address:=r.FormValue("address")
+fmt.Fprintf(w, "Name %s\n", name)
+fmt.Fprintf(w, "Address %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(w,"error, page not found", http.StatusNotFound)
+		return
+	}
+	if r.Method!="GET"{
+       http.Error(w,"Method Not Valid", http.StatusNotFound)
+	   return
+	}
+	 fmt.Fprintf(w, "Hello!")
+}
+
+
+func main (){
+	fileserver:=http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("server now running at port:8080")
+	if err:= http.ListenAndServe(":8080",nil); err!=nil{
+		log.Fatal(err)
+	}
+}
